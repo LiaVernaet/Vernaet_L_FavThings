9202084b16e553abb425e3b73bca3f26f7d3eb20
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-21 16:14:43 -0400

    update

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: "root",
+    password: "",
+    port: 3306,
+    host: "Localhost",
+    database: "db_marvel"
+}
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var connect = require('../utils/sqlConnect');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //get data from db and send it through the route to the views/index.hbs page and render that (including DB data)
+  //res.render('index', { title: 'Express' });
+  connect.query('SELECT name, avatar FROM hero',(err, result)=>{
+
+   if (err){
+     throw err;
+     console.log(err);
+   }else{
+     console.log(result);
+     res.render('index', { avatars: result });
+   }
+  });
 });
 
 module.exports = router;
