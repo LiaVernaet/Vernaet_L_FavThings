8a3f7be81c3959c2086f106027ff3a7bb438964c
LiaVernaet <l_vernaet@fanshaweonline.ca>
2019-03-21 17:03:24 -0400

    edit

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   //get data from db and send it through the route to the views/index.hbs page and render that (including DB data)
   //res.render('index', { title: 'Express' });
-  connect.query('SELECT name, avatar FROM hero',(err, result)=>{
+  connect.query(`SELECT name, avatars FROM hero`,(err, result) => {
 
    if (err){
      throw err;
@@ -14,6 +14,23 @@ router.get('/', function(req, res, next) {
    }else{
      console.log(result);
      res.render('index', { avatars: result });
+   }
+  });
+});
+
+//get one hero's bio information
+
+router.get('/:hero', function(req, res, next) {
+  //get data from db and send it through the route to the views/index.hbs page and render that (including DB data)
+  //res.render('index', { title: 'Express' });
+  connect.query(`SELECT * FROM hero WHERE name="${req.params.hero}"`,(err, result) => {
+
+   if (err){
+     throw err;
+     console.log(err);
+   }else{
+     console.log(result);
+     res.render('bio', { bioData: result });
    }
   });
 });
